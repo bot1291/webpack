56dd1c94a75396a47f8fc0da7c363a1c35d0574f
Igor Raskatov <aura.star@yandex.ru>
2022-10-09 09:15:44 +0300

    Fixed babel loader

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -51,8 +51,13 @@ module.exports = {
     module: {
         rules: [{
             test: /\.js$/,
-            loader: "babel-loader",
-            exclude: "/node_modules/"
+            exclude: "/node_modules/",
+            use : {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"]
+                }
+            }
         }, {
             test: /\.vue$/,
             loader: "vue-loader",
